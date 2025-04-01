2a6dc617e738a49d50885d7d7cfb7c2a58c8dd8a
WesleyZanon <wesleyzanon17@gmail.com>
2025-04-01 11:50:55 -0300

    Initial commit

---
+++ src/main/java/com/example/LUPP_API/LuppApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.LUPP_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LUPP_API/LuppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LUPP_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
