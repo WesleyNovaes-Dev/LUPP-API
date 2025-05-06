5d1e29ec7aadb7b496e2cd20f3107ffd64b633d3
WesleyZanon <wesleyzanon17@gmail.com>
2025-05-06 01:08:24 -0300

    funcional v2

---
+++ src/main/java/com/example/LUPP_API/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.LUPP_API.repositories;
+
+import com.example.LUPP_API.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,17 +2,18 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 
 CREATE TABLE media (
-                       id UUID PRIMARY KEY,
+                       id UUID PRIMARY KEY UNIQUE NOT NULL,
                        title VARCHAR(255) NOT NULL,
                        description TEXT,
                        category VARCHAR(100),
                        media_url VARCHAR(255),
                        creation_date TIMESTAMP,
-                       type VARCHAR(20) NOT NULL
+                       type VARCHAR(20) NOT NULL,
+                       user_id UUID NOT NULL
 );
 
 CREATE TABLE question (
-                          id UUID PRIMARY KEY,
+                          id UUID PRIMARY KEY UNIQUE NOT NULL,
                           media_id UUID NOT NULL,
                           description TEXT NOT NULL,
                           value DOUBLE PRECISION NOT NULL,
@@ -20,9 +21,16 @@ CREATE TABLE question (
 );
 
 CREATE TABLE alternative (
-                             id UUID PRIMARY KEY,
+                             id UUID PRIMARY KEY UNIQUE NOT NULL,
                              question_id UUID NOT NULL,
                              text VARCHAR(255) NOT NULL,
                              is_correct BOOLEAN NOT NULL,
                              CONSTRAINT fk_question FOREIGN KEY(question_id) REFERENCES question(id) ON DELETE CASCADE
+);
+
+CREATE TABLE users (
+                       id UUID PRIMARY KEY UNIQUE NOT NULL,
+                       login TEXT NOT NULL UNIQUE,
+                       password TEXT NOT NULL,
+                       role TEXT NOT NULL
 );
